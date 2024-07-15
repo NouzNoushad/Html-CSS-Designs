f3db5f28e789ec1d53a5d86d9ce1ae8804487612
unknown <nhd7noushadazeez@gmail.com>
2024-07-15 12:04:05 +0530

    about section

--- construction_site/script.js
+++ construction_site/script.js
@@ -1,6 +1,7 @@
 const navBar = document.querySelector(".navbar");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
+const header = document.querySelector("header");
 
 
 menuBtn.onclick = function () {
@@ -9,4 +10,12 @@ menuBtn.onclick = function () {
 
 cancelBtn.onclick = function () {
     navBar.classList.remove("active");
+}
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        header.classList.add("sticky");
+    }else{
+        header.classList.remove("sticky");
+    }
 }
