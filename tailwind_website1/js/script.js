252dd35ab830f5b33379fc411f4c02c243afbffe
unknown <nhd7noushadazeez@gmail.com>
2024-09-05 21:26:21 +0530

    review section

---
+++ tailwind_website1/js/script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.mySwiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    autoplay: {
+
+    }
+});
