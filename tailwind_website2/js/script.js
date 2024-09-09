913aab42b6f1d4dff6c823caf8c47fde278785b8
unknown <nhd7noushadazeez@gmail.com>
2024-09-09 11:51:39 +0530

    header responsive

---
+++ tailwind_website2/js/script.js
@@ -0,0 +1,12 @@
+const navBar = document.querySelector(".nav-bar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const scrollBtn = document.querySelector(".scroll-btn");
+
+menuBtn.onclick = function() {
+    navBar.classList.add("active");
+}
+
+cancelBtn.onclick = function () {
+    navBar.classList.remove("active");
+}
