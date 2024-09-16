8bed963ba12f7bba6c3b8beed9d8990a7bdba9cf
unknown <nhd7noushadazeez@gmail.com>
2024-09-16 11:02:05 +0530

    nav

--- tailwind_website3/js/script.js
+++ tailwind_website3/js/script.js
@@ -6,3 +6,24 @@ const swiper = new Swiper('.mySwiper', {
     },
     autoplay: {}
 });
+
+const navContent = document.querySelector(".navbar .nav-content");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const nav = document.querySelector("nav");
+
+menuBtn.onclick = function () {
+    navContent.classList.add("active");
+}
+
+cancelBtn.onclick = function () {
+    navContent.classList.remove("active");
+}
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+    }
+}
