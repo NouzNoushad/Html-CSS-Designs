ef9a155dd0ae6a8532da9913569cfae5b03033d7
unknown <nhd7noushadazeez@gmail.com>
2024-09-25 10:49:04 +0530

    logo carousel

--- tailwind_website5/js/script.js
+++ tailwind_website5/js/script.js
@@ -15,3 +15,28 @@ const tSwiper = new Swiper('.tSwiper', {
     loop: true,
     autoplay: {},
 });
+
+const cSwiper = new Swiper('.mySwiper', {
+    direction: 'horizontal',
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {},
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        760: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        980: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+    }
+});
