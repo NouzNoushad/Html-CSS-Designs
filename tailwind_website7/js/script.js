c44ea78d3b5eb5fca95fd165e7e10ae7378ca451
unknown <nhd7noushadazeez@gmail.com>
2024-10-02 17:07:35 +0530

    menu section

--- tailwind_website7/js/script.js
+++ tailwind_website7/js/script.js
@@ -5,3 +5,28 @@ const tSwiper = new Swiper('.tSwiper', {
     loop: true,
     autoplay: {},
 });
+
+const cSwiper = new Swiper('.mySwiper', {
+    direction: 'horizontal',
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {},
+    breakpoints: {
+        // when window width is >= 320px
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        760: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        980: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+    }
+});
