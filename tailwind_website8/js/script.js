2cc8cdbb725cb9e2c9f4f9f36ae6134f8b496f30
unknown <nhd7noushadazeez@gmail.com>
2024-10-06 21:03:51 +0530

    nav scroll

--- tailwind_website8/js/script.js
+++ tailwind_website8/js/script.js
@@ -13,3 +13,13 @@ const tSwiper = new Swiper('.tSwiper', {
         prevEl: ".swiper-button-prev",
     }
 });
+
+const nav = document.querySelector("nav");
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+    }
+}
