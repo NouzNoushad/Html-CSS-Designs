d724298a495ecbccc5f95bc17c3088a3c12e4f5e
unknown <nhd7noushadazeez@gmail.com>
2024-10-07 21:59:08 +0530

    project section

--- tailwind_website8/js/script.js
+++ tailwind_website8/js/script.js
@@ -14,6 +14,36 @@ const tSwiper = new Swiper('.tSwiper', {
     }
 });
 
+const cSwiper = new Swiper('.mySwiper', {
+    direction: 'horizontal',
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {},
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+        },
+        // when window width is >= 480px
+        760: {
+            slidesPerView: 2,
+        },
+        // when window width is >= 640px
+        980: {
+            slidesPerView: 3,
+        }
+    }
+});
+
 const nav = document.querySelector("nav");
 
 window.onscroll = function () {
