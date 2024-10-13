3da5a225143943f8a057c20fa2dcbc1bcf2739b7
unknown <nhd7noushadazeez@gmail.com>
2024-10-13 13:42:07 +0530

    client section

--- tailwind_web9/js/script.js
+++ tailwind_web9/js/script.js
@@ -13,3 +13,29 @@ const tSwiper = new Swiper('.tSwiper', {
         prevEl: ".swiper-button-prev",
     }
 });
+
+const cSwiper = new Swiper('.mySwiper', {
+    direction: 'horizontal',
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {},
+    breakpoints: {
+        300: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        490: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        760: {
+            slidesPerView: 4,
+            spaceBetween: 30
+        },
+        980: {
+            slidesPerView: 5,
+            spaceBetween: 40
+        }
+    }
+});
