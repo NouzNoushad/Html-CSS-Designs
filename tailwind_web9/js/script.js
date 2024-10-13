210e9fbc1cf31c517920e42b58aa2b0264f0dae5
unknown <nhd7noushadazeez@gmail.com>
2024-10-13 19:44:45 +0530

    minor fix

--- tailwind_web9/js/script.js
+++ tailwind_web9/js/script.js
@@ -1,3 +1,26 @@
+const nav = document.querySelector("nav");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const scrollBtn = document.querySelector(".scroll-btn")
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("sticky");
+        scrollBtn.style.display = "block";
+    } else {
+        nav.classList.remove("sticky");
+        scrollBtn.style.display = "none";
+    }
+}
+
+menuBtn.onclick = function () {
+    nav.classList.add("active");
+}
+
+cancelBtn.onclick = function () {
+    nav.classList.remove("active");
+}
+
 const tSwiper = new Swiper('.tSwiper', {
     direction: 'horizontal',
     slidesPerView: 1,
