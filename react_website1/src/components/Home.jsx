062eafd139e49ff3efc3194d2720fa2915752530
unknown <nhd7noushadazeez@gmail.com>
2024-10-18 21:54:44 +0530

    scrollbar

--- react_website1/src/components/Home.jsx
+++ react_website1/src/components/Home.jsx
@@ -5,63 +5,65 @@ import 'swiper/css/autoplay';
 
 export default function HomeSection() {
     return (
-        <Swiper
-            modules={[Autoplay]}
-            spaceBetween={0}
-            slidesPerView={1}
-            loop
-            autoplay={{
-                delay: 2500,
-                disableOnInteraction: false,
-            }}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-        >
-            <SwiperSlide className='h-auto sm:h-[90vh] md:h-[100vh] bg-black'>
-                <div className="relative h-full w-full">
-                    <div className="after:absolute after:top-0 after:left-0 after:h-full after:w-full after:bg-[#161616b3] h-full w-full">
-                        <img src="../src/assets/img/carousel-1.jpg" alt="" className='h-full w-full bg-cover bg-center' />
-                    </div>
-                    <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-center space-y-[30px] w-full md:w-[700px] md:mx-auto px-5 xl:px-0">
-                        <h1 className='font-roboto text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[4rem] font-[800] leading-[2.8rem] sm:leading-[3rem] md:leading-[4rem] lg:leading-[4.5rem]'>Best CCTV & Security Solution For You</h1>
-                        <p className='font-open text-[1rem] sm:text-[1.2rem]'>Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
-                        <div className="flex flex-row flex-wrap items-center justify-center gap-[20px]">
-                            <button className='rounded-[30px] w-[160px] h-[50px] bg-[#65B530] font-open font-[500]'>Read More</button>
-                            <button className='rounded-[30px] w-[160px] h-[50px] bg-white font-open text-black font-[500]'>Free Quote</button>
+        <section id='home'>
+            <Swiper
+                modules={[Autoplay]}
+                spaceBetween={0}
+                slidesPerView={1}
+                loop
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide className='h-auto sm:h-[90vh] md:h-[100vh] bg-black'>
+                    <div className="relative h-full w-full">
+                        <div className="after:absolute after:top-0 after:left-0 after:h-full after:w-full after:bg-[#161616b3] h-full w-full">
+                            <img src="../src/assets/img/carousel-1.jpg" alt="" className='h-full w-full bg-cover bg-center' />
+                        </div>
+                        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-center space-y-[30px] w-full md:w-[700px] md:mx-auto px-5 xl:px-0">
+                            <h1 className='font-roboto text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[4rem] font-[800] leading-[2.8rem] sm:leading-[3rem] md:leading-[4rem] lg:leading-[4.5rem]'>Best CCTV & Security Solution For You</h1>
+                            <p className='font-open text-[1rem] sm:text-[1.2rem]'>Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                            <div className="flex flex-row flex-wrap items-center justify-center gap-[20px]">
+                                <button className='rounded-[30px] w-[160px] h-[50px] bg-[#65B530] font-open font-[500]'>Read More</button>
+                                <button className='rounded-[30px] w-[160px] h-[50px] bg-white font-open text-black font-[500]'>Free Quote</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </SwiperSlide>
-            <SwiperSlide className='h-[80vh] sm:h-[90vh] md:h-[100vh] bg-black'>
-                <div className="relative h-full w-full">
-                    <div className="after:absolute after:top-0 after:left-0 after:h-full after:w-full after:bg-[#161616b3] h-full w-full">
-                        <img src="../src/assets/img/carousel-2.jpg" alt="" className='h-full w-full bg-cover bg-center' />
-                    </div>
-                    <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-center space-y-[30px] w-full md:w-[700px] md:mx-auto px-5 xl:px-0">
-                        <h1 className='font-roboto text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[4rem] font-[800] leading-[2.8rem] sm:leading-[3rem] md:leading-[4rem] lg:leading-[4.5rem]'>Smart Security Solution For All Business</h1>
-                        <p className='font-open text-[1rem] sm:text-[1.2rem]'>Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
-                        <div className="flex flex-row flex-wrap items-center justify-center gap-[20px]">
-                            <button className='rounded-[30px] w-[160px] h-[50px] bg-[#65B530] font-open font-[500]'>Read More</button>
-                            <button className='rounded-[30px] w-[160px] h-[50px] bg-white font-open text-black font-[500]'>Free Quote</button>
+                </SwiperSlide>
+                <SwiperSlide className='h-[80vh] sm:h-[90vh] md:h-[100vh] bg-black'>
+                    <div className="relative h-full w-full">
+                        <div className="after:absolute after:top-0 after:left-0 after:h-full after:w-full after:bg-[#161616b3] h-full w-full">
+                            <img src="../src/assets/img/carousel-2.jpg" alt="" className='h-full w-full bg-cover bg-center' />
+                        </div>
+                        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-center space-y-[30px] w-full md:w-[700px] md:mx-auto px-5 xl:px-0">
+                            <h1 className='font-roboto text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[4rem] font-[800] leading-[2.8rem] sm:leading-[3rem] md:leading-[4rem] lg:leading-[4.5rem]'>Smart Security Solution For All Business</h1>
+                            <p className='font-open text-[1rem] sm:text-[1.2rem]'>Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                            <div className="flex flex-row flex-wrap items-center justify-center gap-[20px]">
+                                <button className='rounded-[30px] w-[160px] h-[50px] bg-[#65B530] font-open font-[500]'>Read More</button>
+                                <button className='rounded-[30px] w-[160px] h-[50px] bg-white font-open text-black font-[500]'>Free Quote</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </SwiperSlide>
-            <SwiperSlide className='h-[80vh] sm:h-[90vh] md:h-[100vh] bg-black'>
-                <div className="relative h-full w-full">
-                    <div className="after:absolute after:top-0 after:left-0 after:h-full after:w-full after:bg-[#161616b3] h-full w-full">
-                        <img src="../src/assets/img/carousel-3.jpg" alt="" className='h-full w-full bg-cover bg-center' />
-                    </div>
-                    <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-center space-y-[30px] w-full md:w-[700px] md:mx-auto px-5 xl:px-0">
-                        <h1 className='font-roboto text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[4rem] font-[800] leading-[2.8rem] sm:leading-[3rem] md:leading-[4rem] lg:leading-[4.5rem]'>Innovative Solution For Security System</h1>
-                        <p className='font-open text-[1rem] sm:text-[1.2rem]'>Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
-                        <div className="flex flex-row flex-wrap items-center justify-center gap-[20px]">
-                            <button className='rounded-[30px] w-[160px] h-[50px] bg-[#65B530] font-open font-[500]'>Read More</button>
-                            <button className='rounded-[30px] w-[160px] h-[50px] bg-white font-open text-black font-[500]'>Free Quote</button>
+                </SwiperSlide>
+                <SwiperSlide className='h-[80vh] sm:h-[90vh] md:h-[100vh] bg-black'>
+                    <div className="relative h-full w-full">
+                        <div className="after:absolute after:top-0 after:left-0 after:h-full after:w-full after:bg-[#161616b3] h-full w-full">
+                            <img src="../src/assets/img/carousel-3.jpg" alt="" className='h-full w-full bg-cover bg-center' />
+                        </div>
+                        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-center space-y-[30px] w-full md:w-[700px] md:mx-auto px-5 xl:px-0">
+                            <h1 className='font-roboto text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[4rem] font-[800] leading-[2.8rem] sm:leading-[3rem] md:leading-[4rem] lg:leading-[4.5rem]'>Innovative Solution For Security System</h1>
+                            <p className='font-open text-[1rem] sm:text-[1.2rem]'>Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
+                            <div className="flex flex-row flex-wrap items-center justify-center gap-[20px]">
+                                <button className='rounded-[30px] w-[160px] h-[50px] bg-[#65B530] font-open font-[500]'>Read More</button>
+                                <button className='rounded-[30px] w-[160px] h-[50px] bg-white font-open text-black font-[500]'>Free Quote</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </SwiperSlide>
-        </Swiper>
+                </SwiperSlide>
+            </Swiper>
+        </section>
     )
 }
--- react_website1/src/components/Nav.jsx
+++ react_website1/src/components/Nav.jsx
@@ -1,65 +1,97 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function NavSection() {
+    const [stickyClass, setStickyClass] = useState('relative');
+    const [scrollBtn, setScrollBtn] = useState("hidden");
+
+    useEffect(() => {
+        window.addEventListener('scroll', stickNavbar);
+
+        return () => {
+            window.removeEventListener('scroll', stickNavbar);
+        };
+    }, []);
+
+    const stickNavbar = () => {
+        if (window !== undefined) {
+            let windowHeight = window.scrollY;
+            if (windowHeight > 500) {
+                setStickyClass('fixed bg-white top-0 left-0 z-50 w-full transition-all duration-500');
+                setScrollBtn("fixed bottom-[10px] right-[20px] h-[45px] w-[45px] rounded-[50%] bg-[#65B530] text-white z-50")
+            }
+            else {
+                setStickyClass('relative');
+                setScrollBtn("hidden");
+            }
+        }
+    };
+
     return (
-        <nav>
-            <div className="hidden lg:block bg-[#161616] text-white">
-                <div className="max-w-[1250px] mx-auto px-5 xl:px-0 py-[1rem]">
-                    <div className="flex flex-row items-center justify-between font-open text-[0.9rem] text-[#9B9B9B]">
-                        <div className="flex flex-row items-center gap-[20px]">
-                            <span className='flex flex-row items-center'>
-                                <a href='#' className="h-[32px] w-[32px] bg-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] text-white">
-                                    <i class="fa-solid fa-location-dot"></i>
-                                </a>
-                                123 Street, New York, USA
-                            </span>
-                            <span className='flex flex-row items-center'>
-                                <a href='#' className="h-[32px] w-[32px] bg-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] text-white">
-                                    <i class="fa-solid fa-envelope"></i>
-                                </a>
-                                info@example.com
-                            </span>
-                        </div>
-                        <div className="flex flex-row items-center gap-[20px]">
-                            <span className='flex flex-row items-center'>
-                                <a href='#' className="h-[32px] w-[32px] bg-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] text-white">
-                                    <i class="fa-solid fa-phone"></i>
-                                </a>
-                                +012 345 6789
-                            </span>
-                            <span className='flex flex-row items-center'>
-                                <a href='#' className="h-[32px] w-[32px] bg-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] text-white">
-                                    <i class="fa-solid fa-clock"></i>
-                                </a>
-                                Mon - Fri : 9AM - 9PM
-                            </span>
-                        </div>
-                    </div>
+        <>
+            <a href="#home" className="transition-all duration-500">
+                <div className={scrollBtn}>
+                    <i className="fas fa-arrow-up absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"></i>
                 </div>
-            </div>
-            <div className="bg-white h-[12vh] ">
-                <div className="max-w-[1250px] mx-auto px-5 xl:px-0 py-[1.2rem]">
-                    <div className="flex flex-row items-center justify-between">
-                        <h1 className='text-[1.6rem] text-[#65B530] font-roboto font-[600]'>Securex</h1>
-                        <div className="hidden lg:flex flex-row gap-[30px] links font-open">
-                            <ul className='flex flex-row items-center gap-[30px] font-[500]'>
-                                <li><a href="#home" className='transition-all duration-500 hover:text-[#65B530]'>Home</a></li>
-                                <li><a href="#about" className='transition-all duration-500 hover:text-[#65B530]'>About</a></li>
-                                <li><a href="#service" className='transition-all duration-500 hover:text-[#65B530]'>Service</a></li>
-                                <li><a href="#project" className='transition-all duration-500 hover:text-[#65B530]'>Project</a></li>
-                                <li><a href="#pages" className='transition-all duration-500 hover:text-[#65B530]'>Pages</a></li>
-                                <li><a href="#contact" className='transition-all duration-500 hover:text-[#65B530]'>Contact</a></li>
-                            </ul>
-                            <div className="flex flex-row items-center">
-                                <a href="#" className="h-[35px] w-[35px] bg-[#F5F5F5] text-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] transition-all duration-500 hover:text-white hover:bg-[#65B530]"><i class="fab fa-facebook-f"></i></a>
-                                <a href="#" className="h-[35px] w-[35px] bg-[#F5F5F5] text-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] transition-all duration-500 hover:text-white hover:bg-[#65B530]"><i class="fab fa-twitter"></i></a>
-                                <a href="#" className="h-[35px] w-[35px] bg-[#F5F5F5] text-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] transition-all duration-500 hover:text-white hover:bg-[#65B530]"><i class="fab fa-linkedin-in"></i></a>
-                                <a href="#" className="h-[35px] w-[35px] bg-[#F5F5F5] text-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] transition-all duration-500 hover:text-white hover:bg-[#65B530]"><i class="fab fa-instagram"></i></a>
+            </a>
+            <nav>
+                <div className="hidden lg:block bg-[#161616] text-white">
+                    <div className="max-w-[1250px] mx-auto px-5 xl:px-0 py-[1rem]">
+                        <div className="flex flex-row items-center justify-between font-open text-[0.9rem] text-[#9B9B9B]">
+                            <div className="flex flex-row items-center gap-[20px]">
+                                <span className='flex flex-row items-center'>
+                                    <a href='#' className="h-[32px] w-[32px] bg-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] text-white">
+                                        <i class="fa-solid fa-location-dot"></i>
+                                    </a>
+                                    123 Street, New York, USA
+                                </span>
+                                <span className='flex flex-row items-center'>
+                                    <a href='#' className="h-[32px] w-[32px] bg-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] text-white">
+                                        <i class="fa-solid fa-envelope"></i>
+                                    </a>
+                                    info@example.com
+                                </span>
+                            </div>
+                            <div className="flex flex-row items-center gap-[20px]">
+                                <span className='flex flex-row items-center'>
+                                    <a href='#' className="h-[32px] w-[32px] bg-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] text-white">
+                                        <i class="fa-solid fa-phone"></i>
+                                    </a>
+                                    +012 345 6789
+                                </span>
+                                <span className='flex flex-row items-center'>
+                                    <a href='#' className="h-[32px] w-[32px] bg-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] text-white">
+                                        <i class="fa-solid fa-clock"></i>
+                                    </a>
+                                    Mon - Fri : 9AM - 9PM
+                                </span>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+                <div className={stickyClass + "bg-white h-[12vh]"}>
+                    <div className="max-w-[1250px] mx-auto px-5 xl:px-0 py-[1.2rem]">
+                        <div className="flex flex-row items-center justify-between">
+                            <h1 className='text-[1.6rem] text-[#65B530] font-roboto font-[600]'>Securex</h1>
+                            <div className="hidden lg:flex flex-row gap-[30px] links font-open">
+                                <ul className='flex flex-row items-center gap-[30px] font-[500]'>
+                                    <li><a href="#home" className='transition-all duration-500 hover:text-[#65B530]'>Home</a></li>
+                                    <li><a href="#about" className='transition-all duration-500 hover:text-[#65B530]'>About</a></li>
+                                    <li><a href="#service" className='transition-all duration-500 hover:text-[#65B530]'>Service</a></li>
+                                    <li><a href="#project" className='transition-all duration-500 hover:text-[#65B530]'>Project</a></li>
+                                    <li><a href="#pages" className='transition-all duration-500 hover:text-[#65B530]'>Pages</a></li>
+                                    <li><a href="#contact" className='transition-all duration-500 hover:text-[#65B530]'>Contact</a></li>
+                                </ul>
+                                <div className="flex flex-row items-center">
+                                    <a href="#" className="h-[35px] w-[35px] bg-[#F5F5F5] text-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] transition-all duration-500 hover:text-white hover:bg-[#65B530]"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="#" className="h-[35px] w-[35px] bg-[#F5F5F5] text-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] transition-all duration-500 hover:text-white hover:bg-[#65B530]"><i class="fab fa-twitter"></i></a>
+                                    <a href="#" className="h-[35px] w-[35px] bg-[#F5F5F5] text-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] transition-all duration-500 hover:text-white hover:bg-[#65B530]"><i class="fab fa-linkedin-in"></i></a>
+                                    <a href="#" className="h-[35px] w-[35px] bg-[#F5F5F5] text-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] transition-all duration-500 hover:text-white hover:bg-[#65B530]"><i class="fab fa-instagram"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
