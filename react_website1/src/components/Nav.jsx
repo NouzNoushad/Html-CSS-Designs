137ea10ff409dfecd932b9216e7883c404e767f3
unknown <nhd7noushadazeez@gmail.com>
2024-10-20 11:39:34 +0530

    fix

--- react_website1/src/components/Nav.jsx
+++ react_website1/src/components/Nav.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 export default function NavSection() {
     const [stickyClass, setStickyClass] = useState('relative');
     const [scrollBtn, setScrollBtn] = useState("hidden");
+    const [toggleMenu, setToggleMenu] = useState("");
 
     useEffect(() => {
         window.addEventListener('scroll', stickNavbar);
@@ -25,6 +26,10 @@ export default function NavSection() {
             }
         }
     };
+
+    const handleToggle = () => {
+        setToggleMenu("active");
+    }
 
     return (
         <>
@@ -72,7 +77,7 @@ export default function NavSection() {
                     <div className="max-w-[1250px] mx-auto px-5 xl:px-0 py-[1.2rem]">
                         <div className="flex flex-row items-center justify-between">
                             <h1 className='text-[1.6rem] text-[#65B530] font-roboto font-[600]'>Securex</h1>
-                            <div className="hidden lg:flex flex-row gap-[30px] links font-open">
+                            <div className="hidden lg:flex flex-row gap-[30px] links font-open gap-[30px] tracking-[0.5px] nav-links fixed top-0 right-[-100%] h-full w-full bg-white flex lg:flex-row flex-col items-center justify-center lg:relative lg:top-0 lg:right-0 lg:bg-transparent z-50 transition-all duration-500">
                                 <ul className='flex flex-row items-center gap-[30px] font-[500]'>
                                     <li><a href="#home" className='transition-all duration-500 hover:text-[#65B530]'>Home</a></li>
                                     <li><a href="#about" className='transition-all duration-500 hover:text-[#65B530]'>About</a></li>
@@ -86,6 +91,18 @@ export default function NavSection() {
                                     <a href="#" className="h-[35px] w-[35px] bg-[#F5F5F5] text-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] transition-all duration-500 hover:text-white hover:bg-[#65B530]"><i class="fab fa-twitter"></i></a>
                                     <a href="#" className="h-[35px] w-[35px] bg-[#F5F5F5] text-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] transition-all duration-500 hover:text-white hover:bg-[#65B530]"><i class="fab fa-linkedin-in"></i></a>
                                     <a href="#" className="h-[35px] w-[35px] bg-[#F5F5F5] text-[#65B530] rounded-[50%] flex items-center justify-center mr-[10px] transition-all duration-500 hover:text-white hover:bg-[#65B530]"><i class="fab fa-instagram"></i></a>
+                                </div>
+                                <div className="cancel-btn inline-block lg:hidden absolute top-[15px] right-[20px]">
+                                    <div className="relative bg-white rounded-[5px] h-[45px] w-[45px]">
+                                        <i
+                                            className="fas fa-times absolute top-[50%] left-[50%] text-black text-[1.3rem] translate-x-[-50%] translate-y-[-50%]"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="menu-btn inline-block lg:hidden absolute top-[50%] right-[20px] translate-y-[-50%]" onClick={handleToggle}>
+                                <div className="relative bg-white rounded-[5px] h-[45px] w-[45px]">
+                                    <i
+                                        className="fas fa-bars absolute top-[50%] left-[50%] text-black text-[1.3rem] translate-x-[-50%] translate-y-[-50%]"></i>
                                 </div>
                             </div>
                         </div>
