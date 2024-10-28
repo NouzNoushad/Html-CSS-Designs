85ae4b8ecdb700c22e24dcb9fe67c55c4dc01ef3
unknown <nhd7noushadazeez@gmail.com>
2024-10-28 17:49:26 +0530

    home section right top

--- react_website2/src/components/Home.jsx
+++ react_website2/src/components/Home.jsx
@@ -27,8 +27,26 @@ export default function HomeSection() {
                             <li className='py-[8px]'><a href="#">Fresh Bananas</a></li>
                         </ul>
                     </div>
-                    <div className="w-full lg:w-3/4 bg-blue-100 h-[500px]">
-                        
+                    <div className="w-full lg:w-3/4 h-[500px]">
+                        <div className="flex flex-col md:flex-row items-start md:items-center gap-[30px]">
+                            <div className="grow flex flex-row items-center w-full md:w-auto">
+                                <div className="border border-gray-100 px-[5px] sm:px-[15px] py-[15px] grow">
+                                    <span className='font-[800] pr-6 border-r border-gray-300 hidden md:inline-block'>All Categories <i class="fa-solid fa-caret-down ml-1"></i></span>
+                                    <input type="text" placeholder="What do yo u need?" className='pl-6 grow' />
+                                </div>
+                                <button className='border border-[#7FAD39] bg-[#7FAD39]  px-[20px] sm:px-[30px] py-[15px] text-white uppercase font-bold text-[0.9rem]'>Search</button>
+                            </div>
+                            <div className="flex flex-row items-center gap-[20px]">
+                                <div className="h-[50px] w-[50px] bg-[#F5F5F5] rounded-[50%] flex items-center justify-center text-[#7FAD39]">
+                                    <i class="fa fa-phone"></i>
+                                </div>
+                                <div class="">
+                                    <h5 className='font-bold text-[1.1rem]'>+65 11.188.888</h5>
+                                    <span className='text-[0.9rem] text-[#7d7d7d]'>support 24/7 time</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bottom"></div>
                     </div>
                 </div>
             </div>
--- react_website2/src/components/Nav.jsx
+++ react_website2/src/components/Nav.jsx
@@ -7,17 +7,17 @@ export default function NavSection() {
                 <div className="max-w-[1150px] mx-auto xl:px-0 px-5 py-3 text-[0.9rem]">
                     <div className="flex flex-row items-center justify-between">
                         <ul className='flex flex-row items-center'>
-                            <li className='border-r border-[##1c1c1c] pr-6'><i class="fa fa-envelope mr-1"></i> hello@colorlib.com</li>
+                            <li className='border-r border-gray-300 pr-6'><i class="fa fa-envelope mr-1"></i> hello@colorlib.com</li>
                             <li className='pl-6'>Free Shipping for all Order of $99</li>
                         </ul>
                         <div className="flex flex-row items-center gap-[20px]">
-                            <div class="flex flex-row items-center gap-[20px] border-r border-[#1c1c1c] pr-6">
+                            <div class="flex flex-row items-center gap-[20px] border-r border-gray-300 pr-6">
                                 <a href="#"><i class="fa fa-facebook"></i></a>
                                 <a href="#"><i class="fa fa-twitter"></i></a>
                                 <a href="#"><i class="fa fa-linkedin"></i></a>
                                 <a href="#"><i class="fa fa-pinterest-p"></i></a>
                             </div>
-                            <div class="flex flex-row items-center border-r border-[##1c1c1c] pr-6">
+                            <div class="flex flex-row items-center border-r border-gray-300 pr-6">
                                 <img src="../src/assets/img/language.png" alt="" />
                                 <div className='ml-2'>English</div>
                                 <span class=""></span>
@@ -66,7 +66,7 @@ export default function NavSection() {
                                 <li className='relative'><a href="#"><i class="fa fa-shopping-bag text-[1.2rem]"></i> <span className='absolute top-[-5px] right-[-12px] bg-[#7FAD39] rounded-[50%] text-white text-[0.6rem] font-[800] h-[13px] w-[13px] flex items-center justify-center
                                 '>3</span></a></li>
                             </ul>
-                            <div class="">item: <span className='font-[700]'>$150.00</span></div>
+                            <div className="text-[#7d7d7d]">item: <span className='font-[700] text-black'>$150.00</span></div>
                         </div>
                     </div>
                 </div>
